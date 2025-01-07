376d12173bf05632fb314390c8a66ab95de3adc8
239 <rufwjd12@gmail.com>
2025-01-07 17:51:11 +0900

    Initial commit

---
+++ deliveryApp/src/main/java/com/example/deliveryApp/DeliveryAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliveryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryAppApplication.class, args);
+	}
+
+}
