6c22fa943c1210243e1427d78ddcc16dfc3269b0
Jiwon <may0399@naver.com>
2025-01-08 17:02:09 +0900

    Feat: added to Menu
    수정, 삭제 기능 추가함

--- deliveryApp/src/main/java/com/example/deliveryApp/entity/Menu.java
+++ deliveryApp/src/main/java/com/example/deliveryApp/entity/Menu.java
@@ -15,7 +15,33 @@ public class Menu {
     private String menuName;
     private int price;
 
+    private Boolean isActive;
+
     @ManyToOne
     @JoinColumn(name = "store_id")
     private Store store;
+
+    public Menu(String menuName, int price, Store store) {
+        this.menuName = menuName;
+        this.price = price;
+        this.store = store;
+        this.isActive = true; // 기본적으로 활성 상태로 설정 -> 삭제 시, 메뉴의 상태만 삭제 상태로 변경
+    }
+
+    public Menu() {
+    }
+
+    public void updateMenu(String name, int price) {
+        this.menuName = menuName;
+        this.price = price;
+    }
+
+    public void deleteMenu() {
+        this.isActive = false;
+    }
 }
+
+
+
+
+
