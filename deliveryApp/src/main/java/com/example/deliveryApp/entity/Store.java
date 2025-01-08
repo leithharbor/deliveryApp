e9ebace8a2855b24cdb8f777e627ffc318c127cf
neulbin <msg0447@gmail.com>
2025-01-08 11:41:33 +0900

    Feat: Entitys created
    User, Store, Menu, Order, Review Entity Created

---
+++ deliveryApp/src/main/java/com/example/deliveryApp/entity/Store.java
@@ -0,0 +1,25 @@
+package com.example.deliveryApp.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "store")
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String storeName;
+    private String openClose;
+    private int deliveryMinTime;
+    private boolean isDeletedStore;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+
+}
---
+++ deliveryApp/src/main/java/com/example/deliveryApp/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.deliveryApp.entity;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String userName;
+    private String userEmail;
+    private String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String userType;
+    private boolean isDelete;
+
+}
