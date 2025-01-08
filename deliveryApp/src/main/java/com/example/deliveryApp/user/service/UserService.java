b2d6bc78c9301a6a195fc60bc62cf1c6fb7510af
neulbin <msg0447@gmail.com>
2025-01-08 14:52:26 +0900

    Feat: created UserService
    Added the necessary annotations.
    
    @Service
    @RequsetMapping("/users")
    @RequiredArgsConstructor
    
    Declared a UserRepository in the UserService field.

---
+++ deliveryApp/src/main/java/com/example/deliveryApp/user/service/UserService.java
@@ -0,0 +1,12 @@
+package com.example.deliveryApp.user.service;
+
+import com.example.deliveryApp.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+}
