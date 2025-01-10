e90c9d666be8dbf34db523054fc13b71779ac25b
dornrrl0932 <dornrrl0932@naver.com>
2025-01-10 13:56:31 +0900

    Feat: create OrderController，OrderService，OrderRepository

---
+++ deliveryApp/src/main/java/com/example/deliveryApp/order/controller/OrderController.java
@@ -0,0 +1,13 @@
+package com.example.deliveryApp.order.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+
+}
---
+++ deliveryApp/src/main/java/com/example/deliveryApp/order/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.example.deliveryApp.order.service;
+
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+
+
+}
