da0631a94bda4385e05d797605783139250a711e
dornrrl0932 <dornrrl0932@naver.com>
2025-01-10 14:06:19 +0900

    Feat: add OrderStatus enum class
    - PENDING(대기 중), ACCEPTED(주문 수락), REJECTED(주문 거절), IN_PROGRESS(조리 중), DELIVERING(배달 중), DELIVERED(배달 완료), CANCELLED(주문 취소)

---
+++ deliveryApp/src/main/java/com/example/deliveryApp/order/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.example.deliveryApp.order;
+
+public enum OrderStatus {
+	PENDING,        // 대기 중
+	ACCEPTED,       // 주문 수락
+	REJECTED,       // 주문 거절
+	IN_PROGRESS,    // 조리 중
+	DELIVERING,     // 배달 중
+	DELIVERED,      // 배달 완료
+	CANCELLED       // 주문 취소
+}
