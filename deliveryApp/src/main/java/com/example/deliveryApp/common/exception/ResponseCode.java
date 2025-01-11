bf7d0c01a3c607e03f6c70c8dc1a977d241ea8d3
dornrrl0932 <dornrrl0932@naver.com>
2025-01-12 03:26:40 +0900

    Feat: ResponseCode 예외코드 추가
    - MENU_NOT_FOUND : 메뉴 id 조회 실패
    - ORDER_NOT_FOUND : 주문 id 조회 실패
    - ORDER_AMOUNT_UNFULFILLED : 최소 주문금액 불충족
    - BUSINESS_HOURS_INCONSISTENCY : 영업시간 불일치
    - ORDERSTATUS_SAME_CURRENT : 주문 상태 현재와 동일
    - ORDERSTATUS_CHANGE_IMPOSSIBLE : 주문 상태 변경 불가
    - REASON_NULL_CHECK : 주문 거절 사유 비어있음

--- deliveryApp/src/main/java/com/example/deliveryApp/common/exception/ResponseCode.java
+++ deliveryApp/src/main/java/com/example/deliveryApp/common/exception/ResponseCode.java
@@ -13,8 +13,14 @@ public enum ResponseCode {
     PASSWORD_SAME_AS_BEFORE(HttpStatus.BAD_REQUEST, "바꾸려는 비밀번호가 이전과 동일하거나, 입력한 비밀번호가 서로 다릅니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
     EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "입력하신 아이디를 찾을 수 없습니다. 다시 확인해주세요."),
-    ID_MISMATCH(HttpStatus.UNAUTHORIZED, "권한이 존재하지 않습니다.");
-
+    ID_MISMATCH(HttpStatus.UNAUTHORIZED, "권한이 존재하지 않습니다."),
+    MENU_NOT_FOUND(HttpStatus.NOT_FOUND,"메뉴를 찾을 수 없습니다."),
+    ORDER_NOT_FOUND(HttpStatus.NOT_FOUND,"주문번호를 찾을 수 없습니다."),
+    ORDER_AMOUNT_UNFULFILLED(HttpStatus.FORBIDDEN,"총 주문 금액이 최소 주문 금액보다 적습니다."),
+    BUSINESS_HOURS_INCONSISTENCY(HttpStatus.FORBIDDEN,"지금은 영업시간이 아닙니다."),
+    ORDERSTATUS_SAME_CURRENT(HttpStatus.BAD_REQUEST,"변경하려는 주문상태가 현재와 같습니다."),
+    ORDERSTATUS_CHANGE_IMPOSSIBLE(HttpStatus.BAD_REQUEST,"해당 주문상태로 변경할 수 없습니다."),
+    REASON_NULL_CHECK(HttpStatus.BAD_REQUEST,"주문 거절 시, 거절 사유는 필수 입력해야 합니다.");
 
     private final HttpStatus status;
     private final String message;
