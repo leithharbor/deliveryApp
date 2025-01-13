aca8ab107f5156a085fe0608526d97f6272f3fad
239 <rufwjd12@gmail.com>
2025-01-13 10:27:10 +0900

    feat: review API added

---
+++ deliveryApp/src/main/java/com/example/deliveryApp/Review/controller/ReviewController.java
@@ -0,0 +1,37 @@
+package com.example.deliveryApp.Review.controller;
+
+import com.example.deliveryApp.Review.dto.ReviewRequestDto;
+import com.example.deliveryApp.Review.dto.ReviewResponseDto;
+import com.example.deliveryApp.Review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reviews")
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    // 리뷰 생성
+    @PostMapping
+    public ResponseEntity<ReviewResponseDto> createReview(@RequestBody ReviewRequestDto reviewRequestDto) {
+        ReviewResponseDto reviewResponseDto = reviewService.createReview(reviewRequestDto);
+        return ResponseEntity.ok(reviewService.createReview(reviewRequestDto));
+    }
+
+    // 리뷰 조회
+    /**
+     * @Param storeId
+     * @return
+     */
+    @GetMapping("/{storeId}")
+    public ResponseEntity<List<ReviewResponseDto>> GetAllReviewsAPI(@PathVariable Long storeId ) {
+        List<ReviewResponseDto> reviewResponseDtoList = reviewService.GetAllReviewsService(storeId);
+        return ResponseEntity.ok(reviewResponseDtoList);
+    }
+
+}
